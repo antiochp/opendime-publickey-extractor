06e1e5dbde8cbb0b06dc24fc1ce2ca06ecf10774
antiochp <30642645+antiochp@users.noreply.github.com>
2017-08-24 11:50:19 -0400

    better decoding

--- extract.py
+++ extract.py
@@ -8,7 +8,7 @@ from bitcoin.core.key import CPubKey
 from bitcoin.signmessage import BitcoinMessage
 
 # TODO - read this file from a mounted OpenDime
-with open('./variables.json') as data_file:
+with open('./variables_3.json') as data_file:
     data = json.load(data_file)
 
 pprint(data)
@@ -19,13 +19,16 @@ pprint(parts)
 msg = parts[0]
 sig = parts[2]
 
+pprint(sig)
+sig = sig+"==="
+pprint(sig)
+sig = base64.b64decode(sig, '-_')
+
 msg = BitcoinMessage(msg)
-sig = base64.b64decode(sig+'===') # janky handling of padding
 hash = msg.GetHash()
 
 pubkey = CPubKey.recover_compact(hash, sig)
-
-
+pprint(pubkey)
 hex_bytes = binascii.hexlify(pubkey)
 pprint(hex_bytes)
 hex_str = hex_bytes.decode("ascii")
